2f5929174262cd65685bc867cac10df672060108
Eugene Doan <doan.eugene@gmail.com>
2013-09-22 01:57:31 -0400

    Moved tasks API url pattern to tasks app.

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 
 from . import views
+from .api import TaskResource
+
+task_resource = TaskResource()
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^', include(task_resource.urls, namespace='tasks_api')),
 )
