47593cf6c9c9a533238013931899d29ed21dd490
Eugene Doan <doan.eugene@gmail.com>
2013-10-01 01:21:07 -0400

    Modified production settings to rely on environment variables for DB settings.

--- todo/todo/settings/production.py
+++ todo/todo/settings/production.py
@@ -11,11 +11,11 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'todo',
+        'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
