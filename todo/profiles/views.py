71b0ce65c06e4551f638ba5cd2ad4eade9a62c08
Eugene Doan <doan.eugene@gmail.com>
2013-10-31 23:36:36 -0400

    Added mixins for requiring logins and turning off caching for views that require logins.

--- todo/profiles/views.py
+++ todo/profiles/views.py
@@ -1,12 +1,25 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
 from django.views.generic.base import View
 from django.views.generic.edit import CreateView
 
 from .forms import RegistrationForm
 from .models import Profile
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+class CacheControlMixin(object):
+    @method_decorator(cache_control(must_revalidate=True, no_cache=True, no_store=True))
+    def dispatch(self, request, *args, **kwargs):
+        return super(CacheControlMixin, self).dispatch(request, *args, **kwargs)
 
 class RegistrationView(CreateView):
     model = get_user_model()
