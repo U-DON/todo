097efb393ee2796231d8f83d56d4d3605bfba832
Eugene Doan <doan.eugene@gmail.com>
2013-10-31 23:37:25 -0400

    Fixed registration form to properly set password.

--- todo/profiles/forms.py
+++ todo/profiles/forms.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 from .models import Profile
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
     class Meta:
         model = get_user_model()
         fields = ('email', 'name', 'password')
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
