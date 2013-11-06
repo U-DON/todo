9afbc8160bddb31a109294a03c35154e559f90b3
Eugene Doan <doan.eugene@gmail.com>
2013-11-06 01:18:53 -0500

    Updated registration tests to include timezone field.

--- todo/profiles/tests/test_forms.py
+++ todo/profiles/tests/test_forms.py
@@ -3,15 +3,15 @@ from django.test import TestCase
 from ..forms import RegistrationForm
 
 class RegistrationFormTest(TestCase):
-    def test_register_with_valid_email(self):
-        """Checks that a form with a valid email is valid."""
-        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test'}
+    def test_register_with_valid_form(self):
+        """Checks that a form with valid inputs is valid."""
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test', 'timezone': 'UTC'}
         form = RegistrationForm(data=form_data)
         self.assertTrue(form.is_valid())
 
     def test_register_with_invalid_email(self):
         """Checks that a form with an invalid email is invalid."""
-        form_data = {'email': 'test', 'name': 'test', 'password': 'test'}
+        form_data = {'email': 'test', 'name': 'test', 'password': 'test', 'timezone': 'UTC'}
         form = RegistrationForm(data=form_data)
         self.assertFalse(form.is_valid())
 
@@ -24,5 +24,29 @@ class RegistrationFormTest(TestCase):
         self.assertFalse(form.is_valid())
 
         form_data['email'] = 'test.com'
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_register_with_blank_name(self):
+        """Checks that a form with a blank name is invalid."""
+        form_data = {'email': 'test@test.com', 'name': '', 'password': 'test', 'timezone': 'UTC'}
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_register_with_blank_password(self):
+        """Checks that a form with a blank password is invalid."""
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': '', 'timezone': 'UTC'}
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_register_with_blank_timezone(self):
+        """Checks that a form with a blank timezone is invalid."""
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test', 'timezone': ''}
+        form = RegistrationForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_register_with_invalid_timezone(self):
+        """Checks that a form with an invalid timezone is invalid."""
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test', 'timezone': 'UUTTCC'}
         form = RegistrationForm(data=form_data)
         self.assertFalse(form.is_valid())
--- todo/profiles/tests/test_views.py
+++ todo/profiles/tests/test_views.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 class RegistrationViewTest(TestCase):
     def test_register_with_valid_email(self):
         """Checks that registering with a valid email creates a new user."""
-        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test'}
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:register'), form_data)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(get_user_model().objects.count(), 1)
@@ -16,25 +16,31 @@ class RegistrationViewTest(TestCase):
 
     def test_register_with_missing_email(self):
         """Checks that registering with no email raises an error."""
-        form_data = {'name': 'test', 'password': 'test'}
+        form_data = {'email': '', 'name': 'test', 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:register'), form_data)
         self.assertFormError(response, 'form', 'email', "This field is required.")
 
     def test_register_with_missing_name(self):
         """Checks that registering with no name raises an error."""
-        form_data = {'email': 'test@test.com', 'password': 'test'}
+        form_data = {'email': 'test@test.com', 'name': '', 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:register'), form_data)
         self.assertFormError(response, 'form', 'name', "This field is required.")
 
     def test_register_with_missing_password(self):
         """Checks that registering with no password raises an error."""
-        form_data = {'email': 'test@test.com', 'name': 'test'}
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': '', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:register'), form_data)
         self.assertFormError(response, 'form', 'password', "This field is required.")
 
+    def test_register_with_missing_timezone(self):
+        """Checks that registering with no timezone raises an error."""
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test', 'timezone': ''}
+        response = self.client.post(reverse('profiles:register'), form_data)
+        self.assertFormError(response, 'form', 'timezone', "This field is required.")
+
     def test_register_with_invalid_email(self):
         """Checks that registering with an invalid email returns to registration with an error."""
-        form_data = {'email': 'test', 'name': 'test', 'password': 'test'}
+        form_data = {'email': 'test', 'name': 'test', 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:register'), form_data)
         self.assertFormError(response, 'form', 'email', "Enter a valid email address.")
         self.assertEqual(get_user_model().objects.count(), 0)
@@ -43,9 +49,16 @@ class RegistrationViewTest(TestCase):
         """Checks that registering with an existing email returns to registration with an error."""
         form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test'}
         get_user_model().objects.create_user(**form_data)
+        form_data['timezone'] = 'UTC'
         response = self.client.post(reverse('profiles:register'), form_data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(get_user_model().objects.count(), 1)
+
+    def test_register_with_invalid_timezone(self):
+        """Checks that registering with an invalid timezone raises an error."""
+        form_data = {'email': 'test@test.com', 'name': 'test', 'password': 'test', 'timezone': 'UUTTCC'}
+        response = self.client.post(reverse('profiles:register'), form_data)
+        self.assertFormError(response, 'form', 'timezone', "Select a valid choice. UUTTCC is not one of the available choices.")
 
 class LoginViewTest(TestCase):
     def setUp(self):
@@ -53,20 +66,20 @@ class LoginViewTest(TestCase):
 
     def test_login_with_valid_credentials(self):
         """Checks that logging in with an existing account is successful."""
-        form_data = {'email': self.user.email, 'password': 'test'}
+        form_data = {'email': self.user.email, 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:login'), form_data)
         self.assertEqual(response.status_code, 200)
 
     def test_login_with_blank_email(self):
         """Checks that logging in with a blank email raises an error."""
-        form_data = {'email': '', 'password': 'test'}
+        form_data = {'email': '', 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:login'), form_data)
         self.assertEqual(response.status_code, 200)
         self.assertFormError(response, 'form', 'username', "This field is required.")
 
     def test_login_with_nonexistent_account(self):
         """Checks that logging in with a nonexistent account raises an error."""
-        form_data = {'email': 'john.doe@test.com', 'password': 'test'}
+        form_data = {'email': 'john.doe@test.com', 'password': 'test', 'timezone': 'UTC'}
         response = self.client.post(reverse('profiles:login'), form_data)
         self.assertEqual(response.status_code, 200)
         self.assertFormError(response, 'form', 'username', [])
