4d8b02a3bc5100be421b2d24a133c5e734823042
Eugene Doan <doan.eugene@gmail.com>
2013-11-10 23:49:49 -0500

    Initial list of current tasks now properly updates on refresh after adding a new current task. Used to process the queryset only once.

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -3,7 +3,9 @@ from django.views.generic import ListView
 from .models import Task
 from profiles.views import CacheControlMixin, LoginRequiredMixin
 
-class TaskIndexView(LoginRequiredMixin, CacheControlMixin, ListView):
-    model = Task
-    queryset = Task.objects.current()
+class TaskIndexView(CacheControlMixin, LoginRequiredMixin, ListView):
+    context_object_list = 'tasks'
     template_name = 'tasks/index.html'
+
+    def get_queryset(self):
+        return Task.objects.current()
